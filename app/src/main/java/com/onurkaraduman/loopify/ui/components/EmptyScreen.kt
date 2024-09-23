636142817320421fd709d41ec6604fc25b3b7143
OnurKaraduman <onury.karaduman@gmail.com>
2024-09-24 00:35:52 +0300

    Added Resource sealed class and Empty Screen

---
+++ app/src/main/java/com/onurkaraduman/loopify/ui/components/EmptyScreen.kt
@@ -0,0 +1,37 @@
+package com.onurkaraduman.loopify.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun EmptyScreen(message: String){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Icon(
+            modifier = Modifier.size(150.dp),
+            imageVector = Icons.Default.Warning,
+            contentDescription = "Warning"
+        )
+        Text(
+            text = message,
+            color = Color.Black,
+            fontSize = 20.sp
+        )
+    }
+}
---
+++ app/src/main/java/com/onurkaraduman/loopify/ui/components/RatingBar.kt
@@ -0,0 +1,36 @@
+package com.onurkaraduman.loopify.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+@Composable
+fun RatingBar(
+    modifier: Modifier = Modifier,
+    rating: Int,
+    maxRating: Int = 5
+) {
+    Row(
+        modifier = Modifier.padding(start = 12.dp)
+    ) {
+        for (i in 1..maxRating) {
+            Icon(
+                imageVector = Icons.Filled.Star,
+                contentDescription = "Star $i",
+                modifier = Modifier.size(14.dp),
+                tint = if (i <= rating) Color.Yellow else Color.Gray
+            )
+            if (i < maxRating) {
+                Spacer(modifier = Modifier.width(4.dp))
+            }
+        }
+    }
+}
