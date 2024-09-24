3b4ce61cb5a22628fe00d623d9110708b3df3482
OnurKaraduman <onury.karaduman@gmail.com>
2024-09-24 21:15:39 +0300

    Added Text to Home Screen

--- app/src/main/java/com/onurkaraduman/loopify/ui/home/HomeScreen.kt
+++ app/src/main/java/com/onurkaraduman/loopify/ui/home/HomeScreen.kt
@@ -4,17 +4,23 @@ import ProductCard
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.onurkaraduman.loopify.domain.model.products.ProductsModel
 import com.onurkaraduman.loopify.ui.common.EmptyScreen
 import com.onurkaraduman.loopify.ui.theme.LoopifyTheme
@@ -28,9 +34,12 @@ fun HomeScreen(
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(modifier = Modifier.align(Alignment.Start).padding(start = 8.dp), text = "Recommended For You", fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(8.dp))
         when {
             homeUiState.isLoading -> CircularProgressIndicator()
             homeUiState.productList.isNotEmpty() -> ProductList(
