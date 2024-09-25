53c78dee6569563652481d6d3b3df74a61aaf5cc
OnurKaraduman <onury.karaduman@gmail.com>
2024-09-25 15:08:15 +0300

    Changed Rating bar parameter (Int -> Double)

--- app/src/main/java/com/onurkaraduman/loopify/ui/components/RatingBar.kt
+++ app/src/main/java/com/onurkaraduman/loopify/ui/components/RatingBar.kt
@@ -2,7 +2,6 @@ package com.onurkaraduman.loopify.ui.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -12,20 +11,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+
 @Composable
 fun RatingBar(
     modifier: Modifier = Modifier,
-    rating: Int,
+    rating: Double,
     maxRating: Int = 5
 ) {
     Row(
-        modifier = Modifier.padding(start = 12.dp)
+//        modifier = Modifier.padding(start = 12.dp)
     ) {
         for (i in 1..maxRating) {
             Icon(
                 imageVector = Icons.Filled.Star,
                 contentDescription = "Star $i",
-                modifier = Modifier.size(14.dp),
+                modifier = Modifier.size(18.dp),
                 tint = if (i <= rating) Color.Yellow else Color.Gray
             )
             if (i < maxRating) {
