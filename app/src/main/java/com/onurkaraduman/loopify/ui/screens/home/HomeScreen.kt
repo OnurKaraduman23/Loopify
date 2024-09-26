d91da8d005f2ae93c9ae54c42736034bb60701bb
OnurKaraduman <onury.karaduman@gmail.com>
2024-09-26 18:38:19 +0300

    Product List moved out of Home Screen and into a more global space

--- app/src/main/java/com/onurkaraduman/loopify/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/onurkaraduman/loopify/ui/screens/home/HomeScreen.kt
@@ -1,16 +1,11 @@
 package com.onurkaraduman.loopify.ui.screens.home
 
-import ProductCard
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,9 +15,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.onurkaraduman.loopify.domain.model.products.ProductsModel
-import com.onurkaraduman.loopify.ui.screens.common.EmptyScreen
+import com.onurkaraduman.loopify.ui.screens.common.ErrorScreen
 import com.onurkaraduman.loopify.ui.screens.common.LoadingScreen
+import com.onurkaraduman.loopify.ui.components.ProductList
 import com.onurkaraduman.loopify.ui.theme.LoopifyTheme
 
 
@@ -38,7 +33,12 @@ fun HomeScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.height(12.dp))
-        Text(modifier = Modifier.align(Alignment.Start).padding(start = 8.dp), text = "Recommended For You", fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        Text(
+            modifier = Modifier.align(Alignment.Start).padding(start = 8.dp),
+            text = "Recommended For You",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
         Spacer(modifier = Modifier.height(8.dp))
         when {
             homeUiState.isLoading -> LoadingScreen()
@@ -48,32 +48,12 @@ fun HomeScreen(
                     onClick = { onNavigateDetailScreen(it) })
             }
 
-            else -> EmptyScreen(homeUiState.errorMessage.toString())
+            else -> ErrorScreen(homeUiState.errorMessage.toString())
         }
     }
 
 }
 
-@Composable
-fun ProductList(
-    products: List<ProductsModel>,
-    onClick: (Int) -> Unit
-) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        items(products) { product ->
-            ProductCard(
-                product = product,
-                onClick = { onClick(it) }
-            )
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
@@ -81,9 +61,10 @@ fun PreviewHomeScreen(
     @PreviewParameter(HomeScreenPreviewProvider::class) homeUiState: HomeUiState
 ) {
     LoopifyTheme {
-        HomeScreen(homeUiState = homeUiState,
+        HomeScreen(
+            homeUiState = homeUiState,
             onNavigateDetailScreen = {},
-            )
+        )
     }
 }
 
