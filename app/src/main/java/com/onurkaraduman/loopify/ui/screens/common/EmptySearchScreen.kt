f46f1821d32df67929df4c2b4d2e181013d21ef4
OnurKaraduman <onury.karaduman@gmail.com>
2024-09-26 18:36:21 +0300

    Createdm EmptySearchScreen

---
+++ app/src/main/java/com/onurkaraduman/loopify/ui/screens/common/EmptySearchScreen.kt
@@ -0,0 +1,36 @@
+package com.onurkaraduman.loopify.ui.screens.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.onurkaraduman.loopify.R
+
+@Composable
+fun EmptySearchScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_search_document),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(text = "Search Products")
+
+    }
+}
