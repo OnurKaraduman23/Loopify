857cb451658040b3a3c9c7af23c1d90bda6abf17
OnurKaraduman <onury.karaduman@gmail.com>
2024-09-26 18:38:06 +0300

    Added Product List

---
+++ app/src/main/java/com/onurkaraduman/loopify/ui/components/ProductList.kt
@@ -0,0 +1,34 @@
+package com.onurkaraduman.loopify.ui.components
+
+import ProductCard
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.onurkaraduman.loopify.domain.model.products.ProductsModel
+
+@Composable
+fun ProductList(
+    products: List<ProductsModel>,
+    onClick: (Int) -> Unit
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(products) { product ->
+            ProductCard(
+                product = product,
+                onClick = { onClick(it) }
+            )
+        }
+    }
+}
