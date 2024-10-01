5680d41b2057ca343807f657c147f0aa978486ba
OnurKaraduman <onury.karaduman@gmail.com>
2024-10-01 17:22:43 +0300

    Added Favorites and use cases  dependencies

--- app/src/main/java/com/onurkaraduman/loopify/di/AppModule.kt
+++ app/src/main/java/com/onurkaraduman/loopify/di/AppModule.kt
@@ -1,9 +1,20 @@
 package com.onurkaraduman.loopify.di
 
+import android.app.Application
+import androidx.room.Room
 import com.onurkaraduman.loopify.common.Constants.BASE_URL
+import com.onurkaraduman.loopify.common.Constants.PRODUCT_DATABASE_NAME
+import com.onurkaraduman.loopify.data.local.dao.ProductDao
+import com.onurkaraduman.loopify.data.local.database.ProductsDatabase
 import com.onurkaraduman.loopify.data.repository.MyRepositoryImpl
 import com.onurkaraduman.loopify.data.service.MyApi
 import com.onurkaraduman.loopify.domain.repository.MyRepository
+import com.onurkaraduman.loopify.domain.use_case.local.DeleteProduct
+import com.onurkaraduman.loopify.domain.use_case.local.IsFavoriteProduct
+import com.onurkaraduman.loopify.domain.use_case.local.LocalProductUseCase
+import com.onurkaraduman.loopify.domain.use_case.local.SelectProduct
+import com.onurkaraduman.loopify.domain.use_case.local.SelectProducts
+import com.onurkaraduman.loopify.domain.use_case.local.UpsertProduct
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,7 +39,45 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideMyRepository(api: MyApi): MyRepository = MyRepositoryImpl(api)
+    fun provideMyRepository(
+        api: MyApi,
+        productDao: ProductDao
+    ): MyRepository = MyRepositoryImpl(api, productDao)
 
+    // ROOM Database
+
+    @Provides
+    @Singleton
+    fun provideProductDatabase(
+        application: Application
+    ): ProductsDatabase {
+        return Room.databaseBuilder(
+            context = application,
+            klass = ProductsDatabase::class.java,
+            name = PRODUCT_DATABASE_NAME
+        )
+            .fallbackToDestructiveMigration() // Tablo yapısında bir değişiklik olduğunda eski tb yi siler ve yenisini oluşturur
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideProductDao(
+        productsDatabase: ProductsDatabase
+    ): ProductDao = productsDatabase.productDao
+
+    @Provides
+    @Singleton
+    fun provideLocalUseCases(
+        myRepository: MyRepository
+    ): LocalProductUseCase {
+        return LocalProductUseCase(
+            upsertProduct = UpsertProduct(myRepository),
+            selectProduct = SelectProduct(myRepository),
+            deleteProduct = DeleteProduct(myRepository),
+            selectProducts = SelectProducts(myRepository),
+            isFavoriteProduct = IsFavoriteProduct(myRepository)
+        )
+    }
 
 }
