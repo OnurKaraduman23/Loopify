65f7a53cc6fccaccc4be4d9ae52c2598b9ccbd75
OnurKaraduman <onury.karaduman@gmail.com>
2024-10-10 21:43:06 +0300

    Added SignIn Password toggle Content

--- app/src/main/java/com/onurkaraduman/loopify/ui/screens/sign_in/SignInScreen.kt
+++ app/src/main/java/com/onurkaraduman/loopify/ui/screens/sign_in/SignInScreen.kt
@@ -13,17 +13,29 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
@@ -80,7 +92,11 @@ fun SignInScreen(
         Spacer(modifier = Modifier.height(12.dp))
 
 
-        Image(modifier = Modifier.size(250.dp), painter = painterResource(id= R.drawable.logo), contentDescription = "logo")
+        Image(
+            modifier = Modifier.size(250.dp),
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "logo"
+        )
 
 
         Spacer(modifier = Modifier.height(12.dp))
@@ -117,7 +133,7 @@ fun EmailAndPasswordContent(
     onPasswordChange: (String) -> Unit,
     onSigInClick: () -> Unit
 ) {
-
+    var passwordVisibility by remember { mutableStateOf(false) }
 
     OutlinedTextField(
         value = email,
@@ -129,9 +145,27 @@ fun EmailAndPasswordContent(
 
     OutlinedTextField(
         value = password,
-        maxLines = 1,
-        placeholder = { Text(text = "Password") },
-        onValueChange = onPasswordChange
+        onValueChange = onPasswordChange,
+        label = { Text("Password") },
+        singleLine = true,
+        placeholder = { Text("Enter your password") },
+        visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val imageRes = if (passwordVisibility) {
+                R.drawable.ic_visibility
+            } else {
+                R.drawable.ic_visibility_off
+            }
+            val description = if (passwordVisibility) "Hide password" else "Show password"
+
+            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = imageRes),
+                    contentDescription = description
+                )
+            }
+        }
     )
 
     Spacer(modifier = Modifier.height(16.dp))
