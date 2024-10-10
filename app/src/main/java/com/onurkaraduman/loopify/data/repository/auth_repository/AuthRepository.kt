c368e4e2009e826b5e74fae7f6006a38d5d9e6c7
OnurKaraduman <onury.karaduman@gmail.com>
2024-10-10 21:42:53 +0300

    Added save Username and getUserName functions

--- app/src/main/java/com/onurkaraduman/loopify/data/repository/auth_repository/AuthRepository.kt
+++ app/src/main/java/com/onurkaraduman/loopify/data/repository/auth_repository/AuthRepository.kt
@@ -1,16 +1,24 @@
 package com.onurkaraduman.loopify.data.repository.auth_repository
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.onurkaraduman.loopify.common.Resource
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
-class AuthRepository @Inject constructor(private val auth: FirebaseAuth) {
+class AuthRepository @Inject constructor(
+    private val auth: FirebaseAuth,
+    private val firestore: FirebaseFirestore
+) {
 
-    suspend fun signUp(email: String, password: String): Resource<String> {
+    suspend fun signUp(email: String, password: String, userName: String): Resource<String> {
         return try {
-            val result = auth.createUserWithEmailAndPassword(email,password).await()
-            Resource.Success(result.user?.uid)
+            val result = auth.createUserWithEmailAndPassword(email, password).await()
+            val userId = result.user?.uid
+            userId?.let {
+                saveUserName(userId = userId, userName = userName, email = email)
+            }
+            Resource.Success(userId)
         } catch (e: Exception) {
             Resource.Error(e.message.toString())
         }
@@ -18,8 +26,35 @@ class AuthRepository @Inject constructor(private val auth: FirebaseAuth) {
 
     suspend fun signIn(email: String, password: String): Resource<String> {
         return try {
-            val result = auth.signInWithEmailAndPassword(email,password).await()
+            val result = auth.signInWithEmailAndPassword(email, password).await()
             Resource.Success(result.user?.uid)
+        } catch (e: Exception) {
+            Resource.Error(e.message.toString())
+        }
+    }
+
+    suspend fun saveUserName(userId: String, userName: String, email: String): Resource<Void> {
+        return try {
+            val userMap = hashMapOf(
+                "userId" to userId,
+                "email" to email,
+                "userName" to userName
+            )
+            firestore.collection("users").document(userId).set(userMap).await()
+            Resource.Success(null)
+        } catch (e: Exception) {
+            Resource.Error(e.message.toString())
+        }
+    }
+
+    suspend fun getUserName(userId: String): Resource<String?> {
+        return try {
+            val userDoc = firestore.collection("users").document(userId).get().await()
+            if (userDoc.exists()) {
+                Resource.Success(userDoc.getString("userName"))
+            } else {
+                Resource.Error("User not found")
+            }
         } catch (e: Exception) {
             Resource.Error(e.message.toString())
         }
